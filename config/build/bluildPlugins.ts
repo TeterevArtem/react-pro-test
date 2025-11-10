6d5508bd44e65a7b4419cc6c299bf20ee790133a
Artem Teterev <teterev.artem@gmail.com>
2025-11-10 11:53:57 +0600

    fix baseUrl option for tsconfig.json

--- config/build/bluildPlugins.ts
+++ config/build/bluildPlugins.ts
@@ -1,10 +1,9 @@
-import path from 'path';
 import webpack, { WebpackPluginInstance } from 'webpack'; //to access built-in plugins
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 
-export const buildPlugins = ({paths}:BuildOptions):webpack.WebpackPluginInstance[] => {
+export const buildPlugins = ({paths}:BuildOptions):WebpackPluginInstance[] => {
   return [
     new HtmlWebpackPlugin({
           template: paths.html
